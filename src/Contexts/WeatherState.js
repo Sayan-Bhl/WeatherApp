4e27de90ed0deabf36d5f8453c212519b857ad37
Sayan <sayanbhowal123@gmail.com>
2023-07-23 12:01:41 +0530

    Second Commit

--- src/Contexts/WeatherState.js
+++ src/Contexts/WeatherState.js
@@ -6,7 +6,7 @@ const WeatherState = (props) => {
     const[error, setError]=useState(null)
     const [showAlert, setShowAlert] = useState(true);
     const getWeather = (city) => {
-            fetch(`https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${city}&days=7&aqi=no&alerts=no`)
+            fetch(`http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${city}&days=7&aqi=no&alerts=no`)
             .then((response)=>{
                 if (!response.ok) {
                     throw new Error("City weather data doesn't exist");
